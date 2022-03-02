5321675ede91f279755d173e37321f2a4f69e4bc
diegoathalla <70790082+diegoathalla@users.noreply.github.com>
2022-03-02 09:01:40 +0700

    Add files via upload

---
+++ tugas nomer 1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+main(){
+	int matA[2][2]={4,2,8,10};
+	int matB[2][2]={-5,8,4,-12};
+	cout<<"Matrik A"<<endl;
+	for(int i=0;i<2;i++){
+		for(int j=0;j<2;j++){
+			cout<<matA[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"Matriks B"<<endl;
+		for(int i=0;i<2;i++){
+			for(int j=0;j<2;j++){
+			cout<<matB[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"Press Any Key to Continue..."; getch();
+	system("cls");
+	
+	cout<<"Hasil Penjumlahan Matriks A dan B"<<endl;
+		for(int i=0;i<2;i++){
+			for(int j=0;j<2;j++){
+			cout<<matA[i][j]+matB[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<"Press Any Key to Continue..."; getch();
+	system("cls");
+}
